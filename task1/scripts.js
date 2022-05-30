e95fffa9c337228147d04c3f0c6ae549d8344330
Aadarsh A <aadarsh.ram@gmail.com>
2022-05-30 16:55:54 +0530

    Fixed js

--- task1/scripts.js
+++ task1/scripts.js
@@ -1,113 +1,82 @@
-let delayInMilliseconds = 100;
-let playerScore = 0;
-let playerTurn = 0;
-let flag = 0;
-let level=1;
+let level = 1;
 let currPattern = [];
 let userPattern = [];
-var eleCurrPat = [];
 var elements = Array.from(document.getElementsByTagName('button'));
 elements.splice(0, 1);
-shuffle(elements);
-
-function blink(tile){
-    tile.classList.add('blink');
-}
-
-function blinkEnd(tile){
-    tile.classList.remove('blink');
-}
 
 function shuffle(array) {
   let currentIndex = array.length,  randomIndex;
-
-  // While there remain elements to shuffle.
   while (currentIndex != 0) {
-
-    // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
   }
 
   return array;
 }
 
-function tile4() {
-    levelnew(1);
-    elements.map( button => {
-        button.addEventListener('click', (e) => {
-            console.log("im here");
-            userPattern.push(button.name)
-        });
-    });
-}
-
-function levelnew(level) {
-    currPattern=[];
-    userPattern=[];
-    eleCurrPat=[];
-    tileglow(level);
-    m=setInterval(() => {
-        console.log('userpat', userPattern)
-        console.log('currpat', currPattern)
-        if(userPattern.length>=1){
-            if(userPattern.length==currPattern.length){
-                userPattern.sort()
-                currPattern.sort()
-                if(JSON.stringify(currPattern)==JSON.stringify(userPattern)){
-                    playerScore+=currPattern.length;
-                    level+=1;
-                    clearInterval(m)
-                    levelnew(level);
-                }
-                else{
-                    alert("Game Over");
-                    clearInterval(m)
-                    level=1
-                }
-            }
-        }
-    }, 1000);
-}
-
-function glow(level) {
-  eleCurrPat = [];
-  var temp = 0;
-  while (temp < level) {
-      eleCurrPat.push(elements[temp]);
-      temp += 1;
-  }
-  console.log('elecurrpat', eleCurrPat);
-  if (currPattern.length < temp) {
-    for (var i=0, len = temp; i<len; i++) {
-        blink(eleCurrPat[i]);
-        console.log(eleCurrPat[i]);
-        configTimeout(eleCurrPat[i]);
-        currPattern.push(eleCurrPat[i].name);
-        console.log('i pushed pa'); 
-    }
-  }
-}
-
-function configTimeout(tile) {
-    setTimeout(function() {
-        blinkEnd(tile);
-    },1000);
-}
-
-function tileglow(i) {
-    let a = setInterval(() => {
-        glow(i);
-    }, 1000);
-    setTimeout(function(){
-        clearInterval(a)
-    },i*1000);
-    return null;
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 const myElement = document.getElementById("start");
-myElement.onclick = function() {tile4()};
+
+myElement.addEventListener('click', () => {
+    tileClicks();
+    currPattern = shuffle(elements);
+    blink();
+});
+
+function tileClicks() {
+    var tiles = Array.from(document.getElementsByTagName('button'));
+    tiles.splice(0, 1);
+    for (var i=0, len = tiles.length; i< len; i++) {
+        tiles[i].addEventListener("click", function() {
+            console.log(this);
+            if (!checkClicks(this)) {
+                alert("Game over! \nScore: "+(level-1));
+                window.location.reload();
+                return;
+            }
+        })
+    }
+}
+
+function checkClicks(id) {
+    if (userPattern.includes(id)) {
+        userPattern = userPattern.filter(function (value) {
+            return value != id;
+        });
+        console.log(userPattern);
+        checkGameOver();
+        return true;
+    }
+    return false;
+}
+
+function checkGameOver() {
+    if (userPattern.length === 0) {
+        level = level + 1;
+        blink();
+        return;
+    }
+    if (level === 16) {
+        alert("You won!");
+        window.location.reload();
+    }
+    return;
+}
+
+async function blink() {
+    for (var i = 0; i < level; i++) {
+        var selection = currPattern[i];
+        console.log("HI " + level + "  " + selection);
+        selection.classList.add("blinkup");
+        selection.classList.remove("blinkdown");
+        await sleep(1000);
+        selection.classList.remove("blinkup");
+        selection.classList.add("blinkdown");
+        userPattern.push(selection);
+    }
+    console.log(userPattern);
+}
